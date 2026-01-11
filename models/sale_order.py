54674797814a3a98679c9d6fd5eef07ea8aa1df6
jardel.bernardo <jardel.bernardo@alien-group.com>
2026-01-11 11:52:24 +0100

    [REMOVE] remove the state draft in the field bai_receipt_state Signed-Off-by:<iamjardelprogrammer@gmail.com>

--- models/sale_order.py
+++ models/sale_order.py
@@ -3,6 +3,5 @@ from odoo import models, fields
 class SaleOrderInherit(models.Model):
     _inherit = "sale.order"
     bai_receipt_id = fields.Many2one('bai.receipt')
-    bai_receipt_state = fields.Selection([
-        ('draft', 'Draft'),('bai_valid', 'Valid'),('not_valid', 'Not Valid'),('other_bank', 'Other')
+    bai_receipt_state = fields.Selection([('bai_valid', 'Valid'),('not_valid', 'Not Valid'),('other_bank', 'Other')
     ], default='draft')
